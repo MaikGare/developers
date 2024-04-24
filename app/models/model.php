0dbe7e77c3c75fa9e96643a62fb392d3d1ebae53
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-24 21:32:05 +0200

    bd y model.php creat

---
+++ app/models/model.php
@@ -0,0 +1,11 @@
+<?php 
+
+// declarar class.x
+// declarar variables.
+// declarar el construct.
+// declarar method crear task.
+// declarar method ver task.
+// declarar method editar task.
+// declarar method eliminar task.
+
+?>
