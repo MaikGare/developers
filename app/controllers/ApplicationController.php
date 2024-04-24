5b1616a3bdafa4376a6ebc1f1ff6975a531ff24b
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-24 20:51:53 +0200

    new layout

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,10 +1,15 @@
 <?php
 
+//llamar a la carpte model archivo.
+
 /**
  * Base controller for the application.
  * Add general things in this controller.
  * testestestesadasdasasdad
  */
+
+ //crear a la class app control segun sale abajo y crear la function construct 
+ //$this->model = new Model() para llamar a la function en el models 
 class ApplicationController extends Controller 
 {
 	
