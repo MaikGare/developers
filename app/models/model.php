ac334cf084845fe9c9dd512741550ebfd3d69ad5
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-25 11:19:56 +0200

    +get all data

--- app/models/model.php
+++ app/models/model.php
@@ -1,11 +1,30 @@
 <?php 
 
-// declarar class.x
+// declarar class.
+class Model{
 // declarar variables.
+    protected $dbJson;
+
 // declarar el construct.
+    function __contruct() 
+    {
+        $this->dbJson = __DIR__ . "/db.json";
+    }
+
+// declarar un array que converta los datos json en php.
+    function get_all_data()
+    {
+        $data = [];
+        $json = json_decode(file_get_contents($this->dbJson));
+        foreach ($json as $row) {
+            $data[$row->id] = $row;
+        }
+        return $data;
+    }
+
 // declarar method crear task.
 // declarar method ver task.
 // declarar method editar task.
 // declarar method eliminar task.
-
+}
 ?>
